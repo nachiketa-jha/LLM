76c38f6119783b0b05436c92f8539d0829ae4ce1
nachiketa-jha <nachiketajha2003@gmail.com>
2025-02-20 16:30:33 +0530

    1.2

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
+device = 'cuda' if torch.cuda.is_available else 'cpu'
+print(device)
+
+
+block_size = 8
+batch_size = 4
 
 with open("book.txt", "r", encoding="utf-8")as f:
     text = f.read()
-
 chars = sorted(set(text))
+vocab_size = len(chars)
 
 string_to_int = {ch:i for i, ch in enumerate(chars)}
 int_to_string = {i:ch for i, ch in enumerate(chars)}
@@ -16,9 +24,16 @@ data = torch.tensor(encode(text), dtype=torch.long)
 n = int(0.8*len(data))
 train_data = data[:n]
 val_data = data[n:]
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix=torch.randint(len(data) - block_size, (batch_size,))
+    x= torch.stack([data[i:i+block_size]for i in ix])
+    y= torch.stack([data[i:i+block_size+1]for i in ix])
+    x, y = x.to(device), y.to(device)
 
-block_size = 8
-batch_size = 4
+
+x, y = get_batch('train')
+
 
 x= train_data[:block_size]
 y= train_data[1:block_size+1]
@@ -26,3 +41,24 @@ y= train_data[1:block_size+1]
 for t in range(block_size):
     context = x[:t+1]
     target = y[t]
+
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward_pass(self, index, targets):
+        if targets is None:
+            loss = None
+        else:
+            logits = self.token_embedding_table(index)
+            B,T,C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, target)
+        
+        return loss, logits
+
+
+

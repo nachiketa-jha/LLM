e8a3017f7c01aa1324c773b2e196cbe6f63fe3f1
nachiketa-jha <nachiketajha2003@gmail.com>
2025-02-20 14:44:55 +0530

    1.1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import torch
+
 with open("book.txt", "r", encoding="utf-8")as f:
     text = f.read()
 
@@ -9,3 +11,18 @@ int_to_string = {i:ch for i, ch in enumerate(chars)}
 encode = lambda s: [string_to_int[c] for c in s]
 decode = lambda l: ''.join(int_to_string[i] for i in l) 
 
+data = torch.tensor(encode(text), dtype=torch.long)
+
+n = int(0.8*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+block_size = 8
+batch_size = 4
+
+x= train_data[:block_size]
+y= train_data[1:block_size+1]
+
+for t in range(block_size):
+    context = x[:t+1]
+    target = y[t]
